1b363c1ffadb8ab4d147da3f06c5d2c7804c762a
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2023-09-19 09:12:03 -0600

    Inicial

---
+++ Variables y operadores/tipos de datos.py
@@ -0,0 +1,9 @@
+print(type(15)) # int -> integer -> número entero
+print(type(15.7)) # float -> número real
+print(type(18j)) # complex -> número complejo
+print(type("Hola 15")) # str -> string -> texto
+print(type([15,"hola",20.5])) # list -> lista -> conjunto ordenado y mutable de datos
+print(type((15,"hola",20.5))) # tuple -> tupla -> conjunto ordenado e inmutable de datos
+print(type({"hola": 15, 2: [1,2]})) # dict -> diccionario -> conjunto ordenado y nombrado de datos
+print(type(True)) # bool -> booleano -> Verdadero/Falso
+print(type(False))
