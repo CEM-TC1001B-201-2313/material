f057f46db81134e3d7cc78588a94b21a193e9b50
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2023-10-06 10:47:39 -0600

    Update elementos.py

--- PySimpleGUI/elementos.py
+++ PySimpleGUI/elementos.py
@@ -1,6 +1,20 @@
 import PySimpleGUI as sg
 
+sg.theme("DarkTeal")
+
 def crearVentanaPrincipal():
+    lista_vacunas = [
+        "Pfizer",
+        "Moderna",
+        "AstraZeneca",
+        "Sputnik",
+        "Cansino"
+        ]
+    lista_recomendacion = [
+        "Muy recomendado",
+        "Recomendado",
+        "Nada recomndado"
+        ]
     layout = [
         # Text
         [sg.Text("Mi primera ventana",
@@ -18,13 +32,45 @@ def crearVentanaPrincipal():
          sg.Input(key = "input email")],
         [sg.Text("Contraseña"),
          sg.Input(key = "input contraseña",
-                  password_char="*")]
+                  password_char="*")],
         # Radio
+        [sg.Text("¿Te aplicaste la vacuna?"),
+         sg.Radio("Sí",
+                  key = "radio sí",
+                  group_id = "grupo vacuna",
+                  enable_events = True),
+         sg.Radio("No",
+                  key = "radio no",
+                  group_id = "grupo vacuna",
+                  default = True,
+                  enable_events = True)],
         # Combo
+        [sg.Text("Marca de la vacuna"),
+         sg.Combo(lista_vacunas,
+                  key = "combo vacunas",
+                  default_value = "Pfizer",
+                  disabled = True)],
         # Checkbox
+        [sg.Text("Síntomas"),
+         sg.Checkbox("Fiebre",
+                     key = "checkbox fiebre"),
+         sg.Checkbox("Dolor de cabeza",
+                     key = "checkbox dolor de cabeza"),
+         sg.Checkbox("Tos",
+                     key = "checkbox tos")],
         # Slider
+        [sg.Text("Nivel se satisfacción"),
+         sg.Slider(range=(1,5),
+                   key = "slider satisfacción",
+                   orientation = "horizontal",
+                   default_value = 3)],
         # Spin
+        [sg.Text("¿Recomendarías esta aplicación?"),
+         sg.Spin(lista_recomendacion,
+                 key = "spin recomendación",
+                 initial_value = "Recomendado")],
         # Image
+        [sg.Image("panda.png")]
         ]
     return sg.Window("Mi primer título", layout, finalize=True)
 
@@ -40,5 +86,19 @@ while True:
         email = values["input email"]
         contraseña = values["input contraseña"]
         print(f"Email: {email} - Password: {contraseña}")
+        print(f"Vacuna sí: {values['radio sí']}")
+        print(f"Vacuna no: {values['radio no']}")
+        print(f"Marca de la vacuna: {values['combo vacunas']}")
+        print(f"Fiebre: {values['checkbox fiebre']}")
+        print(f"Dolor de cabeza: {values['checkbox dolor de cabeza']}")
+        print(f"Tos: {values['checkbox tos']}")
+        if values["checkbox fiebre"]:
+            print("Tiene fiebre")
+        print(f"Nivel de satisfacción: {values['slider satisfacción']}")
+        print(f"Recomendación: {values['spin recomendación']}")
     elif event == "botón 2":
         sg.Popup("Ejemplo de popup", title="Título")
+    elif event == "radio sí":
+        window["combo vacunas"].update(disabled = False)
+    elif event == "radio no":
+        window["combo vacunas"].update(disabled = True)
